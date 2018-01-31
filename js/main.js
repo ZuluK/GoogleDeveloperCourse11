addce7dd58230e6cb8d76a018447eb149c2b2704
Richard Spence <richardspence@Richards-MacBook-Air-2.local>
2018-01-31 06:34:31 -0800

    First Commmit

---
+++ js/main.js
@@ -0,0 +1,72 @@
+
+
+//Persistent data structures for efficient immutablity
+
+/*
+ * Programming Quiz: Murder Mystery (3-4)
+ */
+
+// change the value of `room` and `suspect` to test your code
+var room = "ballroom";
+var suspect = "Mr. Parkes";
+
+var weapon = "";
+var solved = false;
+
+if (room === "dining room") {
+    weapon = "knife";
+    solved = true;
+    suspect = "Mr. Parkes";
+} else if (room === "gallery") {
+    weapon = "trophy"
+    solved = true;
+    suspect = "Ms. Van Cleve";
+} else if (room == "ballroom") {
+    weapon = "poison";
+    solved = true;
+    suspect = "Mr. Kalehoff";
+} else {
+    weapon = "pool stick";
+    solved = true;
+    suspect = "Mrs. Sparr";
+
+}
+
+if (solved) {
+	console.log(suspect + " did it in the " + room + " with the " + weapon + "!");
+}
+
+/*
+ * Programming Quiz: Back to School (3-9)
+ */
+
+// change the value of `education` to test your code
+var education = "a Master's degree";
+
+// set the value of this based on a person's education
+var salary;
+
+// your code goes here
+switch (education){
+    case "no high school diploma":
+        salary = 25636;
+        break;
+    case "a high school diploma":
+        salary = 35256;
+        break;
+    case "an Associate's degree":
+        salary = 41496;
+        break;
+    case "a Bachelor's degree":
+        salary = 59124;
+        break;
+    case "a Master's degree":
+        salary = 69732;
+        break;
+    case "a Professional degree":
+        salary = 89960;
+        break;
+    case "a Doctoral degree" :
+        salary = 84396;
+}
+console.log("In 2015" + "," + " a person with " + education + " earned an average of " + "$" + salary.toLocaleString("en-US") + "/year.");
